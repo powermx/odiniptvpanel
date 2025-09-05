90024c8a9607873d860a893a6737e53a768acc94
Power <powermx@gmail.com>
2025-09-05 02:26:05 -0600

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 #kill $(ps aux | grep 'odiniptv' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
 #sleep 1
 #kill $(ps aux | grep 'odiniptv' | grep -v grep | grep -v 'start_services.sh' | awk '{print $2}') 2>/dev/null
